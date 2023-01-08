0c0de0ae4b9bcf4b16c1ce15c2e26d775c000589
truewayy <dldudwls888@gmail.com>
2023-01-08 22:39:21 +0900

    refac: 글로벌 컬러 삭제

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -19,30 +19,4 @@ const GlobalStyle = createGlobalStyle`
 }
 `;
 
-export const Color = (props) => {
-  if (props === "main") {
-    return "#3dd3c4";
-  } else if (props === "border") {
-    return "#e0e0e0";
-  } else if (props === "font") {
-    return "#222222";
-  } else if (props === "sub-font") {
-    return "#515151";
-  } else if (props === "hover") {
-    return "#76d6bc";
-  } else if (props === "accent") {
-    return "#ff7675";
-  }
-};
-
-export const Container = styled.div`
-  z-index: 1;
-  width: 100%;
-
-  @media screen and (max-width: 991px) {
-    padding-right: 30px;
-    padding-left: 30px;
-  }
-`;
-
 export default GlobalStyle;
